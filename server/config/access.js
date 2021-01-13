b69ffd9ff0d9f06785a1f2a9830e3a385930cc73
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-01-13 19:13:33 +0530

    create basic grants

--- server/config/access.js
+++ server/config/access.js
@@ -1,6 +1,68 @@
 const AccessControl = require('accesscontrol');
 const ac = new AccessControl();
 
-let grants = [{
-
-}]
+let grants = [
+    //admin posts
+    {
+        role: 'admin',
+        resource: 'posts',
+        action: 'create:any',
+    },
+    {
+        role: 'admin',
+        resource: 'posts',
+        action: 'read:any',
+    },
+    {
+        role: 'admin',
+        resource: 'posts',
+        action: 'update:any',
+    },
+    {
+        role: 'admin',
+        resource: 'posts',
+        action: 'delete:any',
+    },
+    //admin users
+    {
+        role: 'admin',
+        resource: 'users',
+        action: 'create:any',
+    },
+    {
+        role: 'admin',
+        resource: 'users',
+        action: 'read:any',
+    },
+    {
+        role: 'admin',
+        resource: 'users',
+        action: 'update:any',
+    },
+    {
+        role: 'admin',
+        resource: 'users',
+        action: 'delete:any',
+    },
+    //user post
+    {
+        role: 'users',
+        resource: 'posts',
+        action: 'create:own',
+    },
+    {
+        role: 'users',
+        resource: 'posts',
+        action: 'read:any',
+    },
+    {
+        role: 'users',
+        resource: 'posts',
+        action: 'update:own',
+    },
+    {
+        role: 'users',
+        resource: 'posts',
+        action: 'delete:own',
+    },
+]
