a90349d5b470ef5bab61b2c3b7a5900d45fc1581
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-01-13 19:03:52 +0530

    add cors and body-parser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express')
 const app = express()
 const port = 3000
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Expose-Headers", "X-Total-Count, Content-Range");
+    next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello There!')
