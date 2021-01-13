8b43dc486525a03dae6762917bd00c0e255d3669
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-01-13 22:08:05 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 const AccessControl = require('accesscontrol');
 const { Grants } = require('./config/access.js');
@@ -13,7 +16,9 @@ const useStyles = makeStyles((theme) => ({
     '& > *': {
       margin: theme.spacing(1),
     },
-    backgroundColor: "#FFFFFF"
+  },
+  card: {
+    minWidth: 275,
   },
 }));
 
@@ -33,6 +38,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Card className={classes.root} variant="outlined">
+      <CardContent>
         <form className={classes.root} noValidate autoComplete="off">
           <Input placeholder="Unprotected Name" inputProps={{ 'aria-label': 'description' }} />
           <Input placeholder="Unprotected Age" inputProps={{ 'aria-label': 'description' }} />
@@ -40,6 +47,8 @@ function App() {
             ac.can(userState.user.role).createOwn('posts').granted === true ? false : true
           } inputProps={{ 'aria-label': 'description' }} />
         </form>
+        </CardContent>
+        </Card>
       </header>
     </div>
   );
